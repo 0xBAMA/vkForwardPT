a8e1d569f0304d5b958251415d693727a056970b
jb <jb239812@ohio.edu>
2026-04-06 17:09:27 -0400

    uninitialized memory fix

--- shaders/XYZSpectrum.h
+++ shaders/XYZSpectrum.h
@@ -499,9 +499,10 @@ vec3 wl_rgb ( float lambda ) {
    float x = xyz.x;
    float y = xyz.y;
    float z = xyz.z;
-   vec3 rgb;
+   // vec3 rgb;
    // rgb.r =  3.2404542*x - 1.5371385*y - 0.4985314*z;
    // rgb.g = -0.9692660*x + 1.8760108*y + 0.0415560*z;
    // rgb.b =  0.0556434*x - 0.2040259*y + 1.0572252*z;
-   return clamp( XYZ_2_RGB * rgb, 0., 1.);
+   // return clamp( XYZ_2_RGB * rgb, 0., 1.);
+   return XYZ_2_RGB * xyz;
 }
