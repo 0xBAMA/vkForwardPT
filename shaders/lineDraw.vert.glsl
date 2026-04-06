b5895acfd239a44024b8f36a2346da0bca8d190c
jb <jb239812@ohio.edu>
2026-04-05 21:49:11 -0400

    move some stuff to a lib folder

--- shaders/lineDraw.vert.glsl
+++ shaders/lineDraw.vert.glsl
@@ -4,7 +4,7 @@
 #extension GL_EXT_buffer_reference : require
 
 #include "common.h"
-#include "XYZSpectrum.h"
+#include "lib/XYZSpectrum.h"
 
 struct raySegment {
 	float wavelength;
--- shaders/raytrace.comp.glsl
+++ shaders/raytrace.comp.glsl
@@ -6,8 +6,8 @@
 layout ( local_size_x = 64 ) in;
 
 #include "common.h"
-#include "random.h"
-#include "hg_sdf.glsl"
+#include "lib/random.h"
+#include "lib/hg_sdf.glsl"
 
 struct raySegment {
 	float wavelength;
