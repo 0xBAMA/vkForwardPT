28c7184743bd0489a025d31bcc6d446e32424989
jb <jb239812@ohio.edu>
2026-04-06 16:34:24 -0400

    switch to linear reads for the accumulator

--- shaders/accumulate.comp.glsl
+++ shaders/accumulate.comp.glsl
@@ -8,19 +8,19 @@ layout ( local_size_x = 16, local_size_y = 16 ) in;
 #include "common.h"
 
 // raster color attachment and the accumulator
-layout ( rgba32f, set = 0, binding = 1 ) uniform image2D rasterImage;
+layout ( set = 0, binding = 1 ) uniform sampler2D rasterImage;
 layout ( rgba32f, set = 0, binding = 2 ) uniform image2D accumulator;
 
 void main () {
 	// get the sum
 	const ivec2 loc = ivec2( gl_GlobalInvocationID.xy );
 	vec4 sum =
-		imageLoad( rasterImage, loc ) +
+		texture( rasterImage, ( vec2( loc ) + vec2( 0.5f ) ) / textureSize( rasterImage, 0 ) ) +
 		imageLoad( accumulator, loc );
 
 	if ( GlobalData.reset != 0 )
 		sum = vec4( 0.0f );
 
 	// store the result
-	imageStore( accumulator, loc, sum );
+	 imageStore( accumulator, loc, sum );
 }
