7735efc112c737b61b107f91dd19732e32417476
jb <jb239812@ohio.edu>
2026-04-07 16:58:32 -0400

    AA solution

--- shaders/accumulate.comp.glsl
+++ shaders/accumulate.comp.glsl
@@ -6,16 +6,19 @@
 layout ( local_size_x = 16, local_size_y = 16 ) in;
 
 #include "common.h"
+#include "random.h"
 
 // raster color attachment and the accumulator
 layout ( set = 0, binding = 1 ) uniform sampler2D rasterImage;
 layout ( rgba32f, set = 0, binding = 2 ) uniform image2D accumulator;
 
 void main () {
+	seed = PushConstants.wangSeed + 42069 * gl_GlobalInvocationID.x + 8675309 * gl_GlobalInvocationID.y;
+
 	// get the sum
 	const ivec2 loc = ivec2( gl_GlobalInvocationID.xy );
 	vec4 sum =
-		texture( rasterImage, ( vec2( loc ) + vec2( 0.5f ) ) / textureSize( rasterImage, 0 ) ) +
+		texture( rasterImage, ( vec2( loc ) + vec2( NormalizedRandomFloat(), NormalizedRandomFloat() ) ) / textureSize( rasterImage, 0 ) ) +
 		imageLoad( accumulator, loc );
 
 	if ( GlobalData.reset != 0 )
