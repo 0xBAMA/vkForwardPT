ad3c2208d1b900c7eb2f7bd6ba022e145fd6ff4d
jb <jb239812@ohio.edu>
2026-04-07 00:56:13 -0400

    update function on lights to prepare the iCDF and the preview image, tested working

--- src/lightManager.h
+++ src/lightManager.h
@@ -174,32 +174,191 @@ public:
 
 	void Update () {
 		// compute the current PDF (temporary, not stored outside of this function):
-		std::vector< float > lightPDF;
-			// start with the selected light PDF LUT
-			// apply as many gel filters as there are to apply
+		std::vector< float > PDFScratch;
 
-		// compute the iCDF for the light source, the light manager will need it
+	// start with the selected light PDF LUT, then apply as many gel filters as there are to apply
+
+	// Starting from the selected source PDF, with no filters applied.
+		// We need a curve out of this process representing the filtered light.
+
+		auto LoadPDF = [&] ( int idx ) {
+			PDFScratch.clear();
+			for ( int i = 0; i < 450; i++ ) {
+				PDFScratch.emplace_back( sourcePDFs[ idx ][ i ] );
+			}
+		};
+
+		auto ApplyFilter = [&] ( int idx ) {
+			for ( int i = 0; i < 450; i++ ) {
+				PDFScratch[ i ] *= gelFilters[ idx ][ i ];
+			}
+		};
+
+		auto NormalizePDF = [&] () {
+			float max = 0.0f;
+			for ( int i = 0; i < 450; i++ ) {
+				// first pass, determine the maximum
+				max = std::max( max, PDFScratch[ i ] );
+			}
+			for ( int i = 0; i < 450; i++ ) {
+				// second pass we perform the normalization by the observed maximum
+				PDFScratch[ i ] /= max;
+			}
+		};
+
+		// load the selected source PDF
+		LoadPDF( PDFPick );
+		NormalizePDF();
+
+		// for each selected gel filter
+		for ( int i = 0; i < filterStack.size(); i++ ) {
+			// apply selected gel filter
+			ApplyFilter( filterStack[ i ] );
+
+			// renormalize the PDF
+			NormalizePDF();
+		}
+
+		// we have the final PDF, now let's get CDF and iCDF
+		std::vector< float > cdf;
+		float cumSum = 0.0f;
+		for ( int x = 0; x < PDFScratch.size(); x++ ) {
+			float sum = 0.0f;
+			// increment cumulative sum and CDF
+			cumSum += PDFScratch[ x ];
+			cdf.push_back( cumSum );
+		}
+
+		// normalize the CDF values by the final value during CDF sweep
+		std::vector< glm::vec2 > CDFpoints;
+		for ( int x = 0; x < PDFScratch.size(); x++ ) {
+			// compute the inverse CDF with the aid of a series of 2d points along the curve
+			// adjust baseline for our desired range -> 380nm to 830nm, we have 450nm of data
+			CDFpoints.emplace_back( x + 380, cdf[ x ] / cumSum );
+		}
+
+		iCDF.clear();
+		for ( int x = 0; x < 1024; x++ ) {
+			// each pixel along this strip needs a value of the inverse CDF
+			// this is the intersection with the line defined by the set of segments in the array CDFpoints
+			float normalizedPosition = ( x + 0.5f ) / 1024.0f;
+			for ( int p = 0; p < CDFpoints.size(); p++ )
+				if ( p == ( CDFpoints.size() - 1 ) ) {
+					iCDF.emplace_back( CDFpoints[ p ].x );
+				} else if ( CDFpoints[ p ].y >= normalizedPosition ) {
+					iCDF.emplace_back( remap( normalizedPosition, CDFpoints[ p - 1 ].y, CDFpoints[ p ].y, CDFpoints[ p - 1 ].x, CDFpoints[ p ].x ) );
+					break;
+				}
+		}
+
+		// clearing the image
+		for ( size_t i = 0; i < previewImageSize.width * previewImageSize.height * 4; i+= 4 ) {
+			textureScratch[ i + 0 ] = 0;
+			textureScratch[ i + 1 ] = 0;
+			textureScratch[ i + 2 ] = 0;
+			textureScratch[ i + 3 ] = 255;
+		}
+
+		// at the start here, you have the updated light PDF...
+			// let's go ahead and compute the preview...
+		int xOffset = 0;
+		for ( auto& freqBand : PDFScratch ) {
+			// we know the PDF value at this location...
+			for ( int y = 0; y < previewImageSize.height; y++ ) {
+				float fractionalPosition = 1.0f - float( y ) / float( previewImageSize.height );
+				if ( fractionalPosition < freqBand ) {
+					// we want to use a representative color for the frequency...
+					vec3 c = wavelengthColorLDR( xOffset + 380.0f ) * 255.0f;
+					setPixel( xOffset, y, c );
+				} else {
+					// write clear... maybe a grid pattern?
+					float xWave = sin( xOffset * 0.5f );
+					float yWave = sin( y * 0.5f );
+					const float p = 40.0f;
+					uint8_t v = std::max( 32.0f * pow( ( 16 + 15 * xWave ) / 32.0f, p ), 32.0f * pow( ( 16 + 15 * yWave ) / 32.0f, p ) );
+					setPixel( xOffset, y, vec3( float( v ) ) );
+				}
+			}
+			xOffset++;
+		}
+
+		// we know the sRGB xRite color chip reflectances, and the light emission... we need to convolve to get the color result
+		vec3 color[ 24 ];
+		for ( int chip = 0; chip < 24; chip++ ) {
+			// initial accumulation value
+			color[ chip ] = vec3( 0.0f );
+
+			// we need to iterate over wavelengths and get an average color value under this illuminant
+			for ( int y = 0; y < 450; y++ ) {
+				// color[ chip ] += ( wavelengthColorLinear( 380 + y ) * light.PDFScratch[ y ] ) / 450.0f;
+				color[ chip ] += 3.5f * glm::clamp( wavelengthColorLinear( 380 + y ) * xRiteReflectances[ chip ][ y ] * PDFScratch[ y ], vec3( 0.0f ), vec3( 1.0f ) ) / 450.0f;
+			}
+		}
+
+		for ( int x = 0; x < 6; x++ ) {
+			for ( int y = 0; y < 4; y++ ) {
+				int i = x + 6 * y;
+				int bx = 455;
+				int by = 5;
+				int xS = 13;
+				int yS = 12;
+				int xM = 2;
+				int yM = 2;
+
+				for ( int xo = 0; xo < xS; xo++ ) {
+					for ( int yo = 0; yo < yS; yo++ ) {
+						setPixel( bx + ( xS + xM ) * x + xo, by + ( yS + yM ) * y + yo, glm::vec3( color[ i ].r * 255, color[ i ].g * 255, color[ i ].b * 255 ) );
+					}
+				}
+			}
+		}
+
+		stbi_write_png(
+			std::string( "test.png" ).c_str(),
+			previewImageSize.width,
+			previewImageSize.height,
+			4, // RGBA
+			textureScratch,
+			previewImageSize.width * 4 // stride (bytes per row)
+		);
+
+		// compute the iCDF once you have a final PDF, the light manager will need it
 
 		// compute the state of the preview into the scratch memory
 			// compute the spectral curve preview
 			// compute the xrite color checker
-			// update the Vulkan texture with the new state
 	}
 
 	// spectral distribution
 	int PDFPick{ 0 };
-	std::vector< int > gelFilters;
+	std::vector< int > filterStack;
 
-	// the solved iCDF
+	// the solved iCDF, needed by the light manager
 	std::vector< float > iCDF;
 
-	// ImGui textureID thing
-	// Vulkan texture
-		// texture scratch memory, static allocation
+	// unitless, relative brightness
+	float brightness{ 1.0f };
 
+	// ImGui textureID thing
+	ImTextureID myTextureID;
+
+	// this is just some dimensions I picked, based on the pixel drawings
+	static constexpr VkExtent3D previewImageSize { 554, 64 };
+	uint8_t textureScratch[ 4 * previewImageSize.width * previewImageSize.height ];
+		// texture scratch memory, static allocation -> copied to atlas
+
+	// helper functions for setting pixel color
+	inline void setPixel ( int x, int y, const glm::vec3& color ) {
+		int index = 4 * ( y * previewImageSize.width + x );
+
+		textureScratch[ index + 0 ] = static_cast<uint8_t>( color.r );
+		textureScratch[ index + 1 ] = static_cast<uint8_t>( color.g );
+		textureScratch[ index + 2 ] = static_cast<uint8_t>( color.b );
+		textureScratch[ index + 3 ] = 255; // constant alpha
+	}
 
 private:
-	int myUniqueID;
+	int myUniqueID; // for ImGUI
 };
 
 class LightManager {
