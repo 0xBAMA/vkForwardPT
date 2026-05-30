dc3e7bc246cf90c2b648947821b0381731796115
jb <jb239812@ohio.edu>
2026-05-30 19:29:30 -0400

    strip out unused classes

--- src/timer.h
+++ src/timer.h
@@ -33,93 +33,13 @@ inline std::string fixedWidthTimeString () {
 }
 
 //=============================================================================
-//==== CLI Progress Bar Utility ===============================================
-//=============================================================================
-
-class progressBar {
-public:
-	progressBar () {
-		// set report width
-		// set the label for the progress bar
-		// set the chars that are used to done/undone
-		// set the total amount of shit to do
-		// set the amount of shit done now
-		// set the time that the process started? or skip timing entirely for now?
-	}
-
-	float done = 0.0f;
-	float total = 1.0f;
-
-	float getFraction () {
-		return std::clamp( float( done ) / float( total ), 0.0f, 1.0f );
-	}
-
-	string getFilledBarPortion () {
-		stringstream ss;
-		const float frac = getFraction();
-		int numFill = std::floor( reportWidth * frac ) - 1;
-		for( int i = 0; i <= numFill; i++ )
-			ss << doneChar;
-		return ss.str();
-	}
-
-	string getEmptyBarPortion () {
-		stringstream ss;
-		const float frac = getFraction();
-		int numFill = std::floor( reportWidth * frac ) - 1;
-		for ( int i = 0; i < reportWidth - numFill - 1; i++ )
-			ss << undoneChar;
-		return ss.str();
-	}
-
-	string getPercentageString () {
-		stringstream ss;
-		const float frac = getFraction();
-		ss << fixedWidthNumberString( std::floor( 100.0f * frac ), 3, ' ' ) << "." << fixedWidthNumberString( int( 10000.0f * frac ) - std::floor( 100.0f * frac ) * 100, 2 );
-		return ss.str();
-	}
-
-	string currentState () {
-		stringstream ss;
-		// const float frac = getFraction();
-		// int numFill = std::floor( reportWidth * frac ) - 1;
-
-		// draw the bar
-		ss << label << "[";
-		ss << getFilledBarPortion();
-		ss << getEmptyBarPortion();
-		ss << "] ";
-
-		// and report the percentage
-		ss << getPercentageString() << "%";
-		return ss.str();
-	}
-
-	void writeCurrentState () {
-		// calculate completion percentage
-		cout << "\r" << currentState() << "                       ";
-
-		// and timing if desired
-		// if ( reportTime )
-			// cout << " in " << Tock() / 1000.0f << "s" << std::flush;
-	}
-
-	char doneChar = '=';
-	char undoneChar = '.';
-	int reportWidth = 64;
-	bool reportTime = false;
-
-	std::string label = std::string( " Progress: " );
-};
-
-//=============================================================================
 //==== OpenGL Timer Query Wrapper =============================================
 //=============================================================================
-
 struct queryPair_GPU {
 	queryPair_GPU ( string s ) : label( s ) {}
 	string label;
-	int index = -1;
+	int tStampStart = -1;
+	int tStampEnd = -1;
 	float result = 0.0f;
 };
 
@@ -228,29 +148,20 @@ public:
 	}
 };
 
-// Timing for the initialization code, similar to scoped timers but outputs to CLI
-class Block {
-const int reportWidth = 40;
-public:
-	std::string myName;
-	std::chrono::time_point<std::chrono::steady_clock> tStart;
-	Block( string blockName ) {
-		myName = blockName;
-		tStart = std::chrono::steady_clock::now();
-		cout << T_BLUE << "    " << blockName << RESET << "... " << endl;
-	}
 
-	~Block() {
-		const float timeInMS = std::chrono::duration_cast< std::chrono::microseconds >( std::chrono::steady_clock::now() - tStart ).count() / 1000.0f;
-		const float wholeMS = int( std::floor( timeInMS ) );
-		const float partialMS = int( ( timeInMS - wholeMS ) * 1000.0f );
-		cout << "        ";
-		// for ( unsigned int i = 0; i < reportWidth - myName.size(); i++ ) {
-		for ( int i = 0; i < reportWidth; i++ ) {
-			cout << ".";
-		}
-		cout << T_GREEN << "done." << T_RED << " ( " << std::setfill( ' ' ) << std::setw( 6 ) << wholeMS << "."
-			<< std::setw( 3 ) << std::setfill( '0' ) << partialMS << " ms )" << RESET << newline;
+// better design to just reuse the code
+class scopedTimer {
+public:
+	unscopedTimer t;
+	scopedTimer ( string label ) : t( label ) {
+		t.tick();
+	}
+	~scopedTimer () {
+		t.tock();
+
+		// GPU query finish
+		timerManager->queries_GPU.emplace_back( t.q );
+		timerManager->queries_CPU.emplace_back( t.c );
 	}
 };
 
