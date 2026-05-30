63123b80993f876bf25118e2b950784266f5660b
jb <jb239812@ohio.edu>
2026-05-30 19:30:08 -0400

    refactor scoped timer, based on unscoped timer, so it's written once

--- src/timer.h
+++ src/timer.h
@@ -10,6 +10,7 @@
 
 using std::cout;
 using std::endl;
+using std::string;
 
 #include <vk_types.h>
 #include <vk_descriptors.h>
@@ -48,103 +49,121 @@ struct queryPair_CPU {
 	string label;
 	std::chrono::time_point< std::chrono::system_clock > tStart;
 	std::chrono::time_point< std::chrono::system_clock > tStop;
-	float result;
+	float result = 0.0f;
 };
 
-class timerManager {
+struct timingResult_t {
+	string label;
+	float tStartCPU, tStopCPU;
+	float tStartGPU, tStopGPU;
+};
+
+class timerManager_t {
 public:
+	// storing the queries
 	std::vector < queryPair_GPU > queries_GPU;
 	std::vector < queryPair_CPU > queries_CPU;
-	void gather () {
-		for ( auto& q : queries_GPU ) {
-			GLint timeAvailable = 0;
-			while ( !timeAvailable ) { // wait on the most recent of the queries to become available
-				glGetQueryObjectiv( q.queryID[ 1 ], GL_QUERY_RESULT_AVAILABLE, &timeAvailable );
-			}
 
-			GLuint64 startTime, stopTime; // get the query results, since they're both ready
-			glGetQueryObjectui64v( q.queryID[ 0 ], GL_QUERY_RESULT, &startTime );
-			glGetQueryObjectui64v( q.queryID[ 1 ], GL_QUERY_RESULT, &stopTime );
-			glDeleteQueries( 2, &q.queryID[ 0 ] ); // and then delete them
+	// last frame's data, prepped for output
+	std::vector < timingResult_t > timingResults;
 
-			// get final operation time in ms, from difference of nanosecond timestamps
-			q.result = ( stopTime - startTime ) / 1000000.0f;
-		}
+	// Vulkan handles required for timestamps...
+	VkDevice * device;
+	VkQueryPool * pool;
+	VkCommandBuffer * cmd;
+
+	// nanosecond division
+	int timestampPeriod;
+
+	// need this value in one place, so that queries can use it
+	int currentIndex = 0;
+
+	// t0 is timestamp for the frame's beginning
+	std::chrono::time_point< std::chrono::system_clock > t0;
+
+	// memory for storing the result from Vulkan
+	static constexpr int maxQueries = 128;
+	std::array< uint64_t, maxQueries * 2 > timestampBuffer {};
+
+	void reset () {
+		// clear the query lists + reserve max available storage
+		queries_GPU.clear(); queries_GPU.reserve( maxQueries );
+		queries_CPU.clear(); queries_CPU.reserve( maxQueries );
+
+		// reset indexing
+		currentIndex = 0;
+
+		// reset the zero point for this frame
+		t0 = std::chrono::system_clock::now();
+
+		// and also reset the pool
+		vkCmdResetQueryPool( *cmd, *pool, 0, static_cast< uint32_t >( maxQueries ) );
 	}
 
-	void clear () { // prepare for next frame
-		queries_GPU.clear();
-		queries_CPU.clear();
+	void gather () {
+		// get the timestamp data back from Vulkan
+		vkGetQueryPoolResults( *device, *pool, 0, maxQueries,
+			2 * maxQueries * sizeof( uint64_t ), &timestampBuffer, 2 * sizeof( uint64_t ),
+			VK_QUERY_RESULT_64_BIT | VK_QUERY_RESULT_WITH_AVAILABILITY_BIT );
+
+		// calculate the floating point deltas in ms (CPU + GPU)
+		timingResults.clear();
+		timingResults.reserve( currentIndex );
+		float GPUOffset = 0.0f;
+		for ( int i = 0; i < queries_GPU.size(); i++ ) {
+			timingResult_t t;
+			t.label = queries_GPU[ i ].label;
+
+			// GPU timing
+			t.tStartGPU = GPUOffset;
+			GPUOffset += float( timestampBuffer[ 2 * queries_GPU[ i ].tStampEnd ] - timestampBuffer[ 2 * queries_GPU[ i ].tStampStart ] ) * ( timestampPeriod / 1000000.0f );
+			t.tStopGPU = GPUOffset;
+
+			// CPU timing
+			t.tStartCPU = std::chrono::duration_cast<std::chrono::microseconds>(  queries_CPU[ i ].tStart - t0 ).count() / 1000.0f;
+			t.tStopCPU = std::chrono::duration_cast<std::chrono::microseconds>(  queries_CPU[ i ].tStop - t0 ).count() / 1000.0f;
+
+			// fmt::print( "{} runs {}ms to {}ms (CPU) and {}ms to {}ms (GPU)\n", t.label, t.tStartCPU, t.tStopCPU, t.tStartGPU, t.tStopGPU );
+
+			timingResults.push_back( t );
+		}
+		fmt::print( "\n\n" );
 	}
 };
 
+// this needs to be available globally
+inline timerManager_t* timerManager;
+
+// basic tick/tock timer
 class unscopedTimer {
 public:
 	queryPair_GPU q;
-
-	std::chrono::time_point< std::chrono::steady_clock > tStart;
-	std::chrono::time_point< std::chrono::steady_clock > tStop;
-
-	unscopedTimer () : q( " " ) {}
-	void tick () { // start the timers
-		// GPU
-		glGenQueries( 2, &q.queryID[ 0 ] );
-		glQueryCounter( q.queryID[ 0 ], GL_TIMESTAMP );
-
-		// CPU
-		tStart = std::chrono::steady_clock::now();
-	}
-
-	void tock () { // end the timers
-		// GPU
-		glQueryCounter( q.queryID[ 1 ], GL_TIMESTAMP );
-		GLint timeAvailable = 0;
-		while ( !timeAvailable ) { // wait on the most recent of the queries to become available
-			glGetQueryObjectiv( q.queryID[ 1 ], GL_QUERY_RESULT_AVAILABLE, &timeAvailable );
-		}
-
-		GLuint64 startTime, stopTime; // get the query results, since they're both ready
-		glGetQueryObjectui64v( q.queryID[ 0 ], GL_QUERY_RESULT, &startTime );
-		glGetQueryObjectui64v( q.queryID[ 1 ], GL_QUERY_RESULT, &stopTime );
-		glDeleteQueries( 2, &q.queryID[ 0 ] ); // and then delete them
-
-		// get final operation time in ms, from difference of nanosecond timestamps
-		timeGPU = ( stopTime - startTime ) / 1000000.0f;
-
-		// CPU
-		tStop = std::chrono::steady_clock::now();
-		timeCPU = std::chrono::duration_cast<std::chrono::microseconds>( tStop - tStart ).count() / 1000.0f;
-	}
-
-	// values in ms
-	float timeGPU;
-	float timeCPU;
-};
-
-inline timerManager* timerQueries;
-inline vkQueryPool* queries;
-
-class scopedTimer {
-public:
 	queryPair_CPU c;
-	queryPair_GPU q;
-	scopedTimer ( string label ) : c ( label ), q ( label ) {
-		// GPU query prep
-		glGenQueries( 2, &q.queryID[ 0 ] );
-		glQueryCounter( q.queryID[ 0 ], GL_TIMESTAMP );
 
-		// CPU query prep
+	string s;
+
+	unscopedTimer ( string label ) : q( label ), c( label ), s( label ) {}
+
+	void tick () { // explicitly start the timer / insert initial timestamp
+		// GPU timestamp
+		int idx = timerManager->currentIndex;
+		vkCmdWriteTimestamp( * ( timerManager->cmd ), VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT, * ( timerManager->pool ), idx );
+		q.tStampStart = idx;
+		timerManager->currentIndex++;
+
+		// CPU timestamp
 		c.tStart = std::chrono::system_clock::now();
 	}
-	~scopedTimer () {
-		// GPU query finish
-		glQueryCounter( q.queryID[ 1 ], GL_TIMESTAMP );
-		timerQueries->queries_GPU.push_back( q );
 
-		// CPU query finish
+	void tock () { // explicitly end the timer / insert final timestamp
+		// GPU timestamp
+		int idx = timerManager->currentIndex;
+		vkCmdWriteTimestamp( * ( timerManager->cmd ), VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT, * ( timerManager->pool ), idx );
+		q.tStampEnd = idx;
+		timerManager->currentIndex++;
+
+		// CPU timestamp
 		c.tStop = std::chrono::system_clock::now();
-		c.result = std::chrono::duration_cast<std::chrono::microseconds>( c.tStop - c.tStart ).count() / 1000.0f;
-		timerQueries->queries_CPU.push_back( c );
 	}
 };
 
