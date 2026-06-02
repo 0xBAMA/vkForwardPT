c1827b97cd1d3260fa6a4bdf5f4486ad143bf1b9
jb <jb239812@ohio.edu>
2026-06-02 13:31:45 -0400

    2D SDF primitives

--- shaders/gridPrecomputePrimitive.comp.glsl
+++ shaders/gridPrecomputePrimitive.comp.glsl
@@ -7,6 +7,12 @@ layout ( local_size_x = 8, local_size_y = 8 ) in;
 
 #include "common.h"
 
+// line segment distance
+// https://www.shadertoy.com/view/3tdSDj
+
+// circular arc distance
+// https://www.shadertoy.com/view/3cXSRf
+
 void main () {
 	// determine location on the image
 	uvec2 loc = gl_GlobalInvocationID.xy;
