daf8550a570bdb911634484733c3e7e3b7b158ff
jb <jb239812@ohio.edu>
2026-06-08 13:32:46 -0400

    imgui configured to use volk for function loading

--- src/third_party/imgui/imconfig.h
+++ src/third_party/imgui/imconfig.h
@@ -14,6 +14,9 @@
 
 #pragma once
 
+#define IMGUI_IMPL_VULKAN_USE_VOLK
+#define IMGUI_IMPL_VULKAN_VOLK_FILENAME    "../volk/volk.h"       // Default
+
 //---- Define assertion handler. Defaults to calling assert().
 // - If your macro uses multiple statements, make sure is enclosed in a 'do { .. } while (0)' block so it can be used as a single statement.
 // - Compiling with NDEBUG will usually strip out assert() to nothing, which is NOT recommended because we use asserts to notify of programmer mistakes.
